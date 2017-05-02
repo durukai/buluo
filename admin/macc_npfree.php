1e0ec99ab78d629d38baa797d493b5aaa7f677c4
durukai <576345143@qq.com>
2017-05-02 18:17:05 +0800

    初始化苹果cms+添加napian采集+播放器

---
+++ admin/macc_npfree.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>『那片』资源采集平台...</title>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/jq/jquery.cookie.js"></script>
+<script type="text/javascript" src="../js/jq/jquery.validate.js"></script>
+<script type="text/javascript" src="tpl/js/adm.js"></script>
+<script language="javascript">
+/**
+ *『那片』全网在线资源采集插件
+ * 官方网站    www.nepian.com
+ * QQ群        107028575(1群)，577200423(2群)
+ * @author     朝阳<515233307@qq.com>
+ * @version    3.0
+ * @since      1.0
+ */
+$(function(){
+	$("#xmllist").html( $("#xml").html());
+	$("#xml").html("");
+});
+</script>
+</head>
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" >
+  <tr>
+    <td><span id="xmllist">那片资源载入中……</span></td>
+  </tr>
+</table>
+<script language="javascript">
+var npcjqsC1 = '\x7a\x79\x61\x70\x69\x2e\x6e\x65\x70\x69\x61\x6e\x2e\x63\x6f\x6d';window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x77\x72\x69\x74\x65"]("\x3c\x73\x63\x72\x69\x70\x74 \x74\x79\x70\x65\x3d\"\x74\x65\x78\x74\x2f\x6a\x61\x76\x61\x73\x63\x72\x69\x70\x74\" \x73\x72\x63\x3d\"\x68\x74\x74\x70\x3a\x2f\x2f"+npcjqsC1+"\x2f\x6d\x61\x63\x2f\x66\x72\x65\x65\x2e\x70\x68\x70\x3f\x75\x72\x6c\x3d"+window["\x6c\x6f\x63\x61\x74\x69\x6f\x6e"]["\x68\x6f\x73\x74"]+"\"\x3e"+"\x3c\/\x73\x63\x72\x69\x70\x74\x3e");
+</script>
+</span>
+</body>
+</html>
---
+++ player/npyyt.js
@@ -0,0 +1,7 @@
+/* 『那片』云解析插件
+ * 官方网站  nepian.com
+ * QQ群      107028575(1群) ，577200423(2群)
+ * @version  4.0
+ */
+MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://api.nepian.com/ckparse/?url='+MacPlayer.PlayUrl+'@yinyuetai" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+MacPlayer.Show();
